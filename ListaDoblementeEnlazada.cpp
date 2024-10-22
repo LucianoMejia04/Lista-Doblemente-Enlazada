cf63e0c7f9df67380f84aed87d8cad3648f36809
Luciano Enrique Mejia Pretell <lmejiapretell@gmail.com>
2024-10-22 10:51:46 -0600

    chi

--- ListaDoblementeEnlazada.cpp
+++ ListaDoblementeEnlazada.cpp
@@ -39,8 +39,19 @@ void imprimirList(Node*& head) {
 
 }
 
-void addInicio(Node* head, int x) {
+void addInicio(Node*& head, int x) {
+    Node* newnode = new Node;
+    newnode->dato = x;
+    newnode->next = nullptr;
+    newnode->prev = nullptr;
 
+    if (head == nullptr) {
+        head = newnode;
+    } else {
+        newnode->next = head;
+        head->prev = newnode;
+        head = newnode;
+    }
 }
 
 Node* buscar(Node* head, int d) {
@@ -52,6 +63,7 @@ Node* buscar(Node* head, int d) {
             temp1 = temp1->next;
         }
 }
+
 void agregarAntDesp(Node* head, int numIngre, int despAntes , char x) {
 
     Node* newnode = new Node;
@@ -81,10 +93,74 @@ void agregarAntDesp(Node* head, int numIngre, int despAntes , char x) {
     }
 
 }
+void eliminarNumero(Node*& head) {
+    int op;
+    do {
+        cout << "1. Eliminar primer numero";
+        cout << "2. Eliminar x numero";
+        cout << "3. Eliminar ultimo numero";
+        cout << "4. Salir";
+        cin >> op;
+
+        switch (op) {
+        case 1:
+            Node * temp1 = head;
+            Node* temp2 = head->next;
+
+            temp1->next = nullptr;
+            temp2->prev = nullptr;
+
+            head = temp2;
+
+            delete temp1;
+            break;
+
+        case 2:
+            int x;
+            cout << "Ingrese el numero a eliminar: "; cin >> x;
+
+            Node* temp3 = buscar(head, x);
+            temp3->prev->next = temp3->next;
+            temp3->next->prev = temp3->prev;
+            temp3->next = nullptr;
+            temp3->prev = nullptr;
+
+            delete temp3;
+            break;
+
+        case 3:
+            Node * temp4 = head;
+            while (temp4 != nullptr) {
+                temp4 = temp4->next;
+            }
+
+            temp4->prev->next = nullptr;
+            temp4->prev = nullptr;
+
+            delete temp4;
+
+            break;
+
+        default:
+            cout << "Opcion no validad intente denuevo" << endl;
+            system("pause");
+            break;
+        }
+    } while (op != 4);
+}
+
+
+void eliminarTodo(Node*& head) {
+
+}
+
+void editarNumero(Node*& head) {
+
+}
 
 int main()  {
 
-	int n, m;
+    int n, m, o;
 	Node* head = nullptr;
 	cout << "Ingrese el n de la lista: "; cin >> n;
 	cout << endl;
@@ -97,16 +173,21 @@ int main()  {
     system("cls");
     do {
         system("cls");
-        int o;
         cout << endl;
         cout << "            MENU             " << endl;
         cout << endl;
         cout << "1. Agregar al inicio" << endl;
         cout << "2. Agregar al final" << endl;
-        cout << "3. Imprimir Lista" << endl;
+        cout << "3. Imprimir lista" << endl;
         cout << "4. Agregar numero despues de" << endl;
         cout << "5. Agregar numero antes de" << endl;
+        cout << "6. Eliminar numero" << endl;
+        cout << "7. Eliminar todo" << endl;
+        cout << "8. Editar numero" << endl;
+        cout << "9. Salir" << endl;
+
         cout << endl << "\t\t";
+        imprimirList(head);
         cin >> o;
 
         switch (o) {
@@ -157,9 +238,28 @@ int main()  {
             cout << endl;
             system("pause");
             break;
+
+        case 6:
+            system("cls");
+            eliminarNumero(head);
+            break;
+
+        case 7:
+            system("cls");
+
+            break;
+
+        case 8:
+            system("cls");
+
+            break;
+
+        default:
+            cout << "Opcion no valida, por favor intente denuevo" << endl;
+            system("pause");
+            break;
         }
-    } while (o != 6);
+    } while (o != 9);
 
 
-	imprimirList(head);
 }
